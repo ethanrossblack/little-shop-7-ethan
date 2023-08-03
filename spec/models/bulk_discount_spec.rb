459752745944ad346783f3264e2a85e3b2724d74
Ethan Black <ethanrossblack@gmail.com>
2023-08-03 17:42:35 -0600

    feat: add bulk_discount model, table, and migration

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe BulkDiscount, type: :model do
     it {should belong_to :merchant}
   end
 
-  describe "validations" do
-    it {should validate_presence_of :percent_discount}
+  describe "Validations" do
+    it {should validate_presence_of :discount}
     it {should validate_presence_of :quantity}
   end
 end
