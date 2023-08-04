6dcc4bcfc6c92e758778c68c611815fa67e60270
Ethan Black <ethanrossblack@gmail.com>
2023-08-04 15:45:54 -0600

    feat: add create new discount button to discount index page

--- spec/features/merchants/bulk_discounts/new_spec.rb
+++ spec/features/merchants/bulk_discounts/new_spec.rb
@@ -29,13 +29,3 @@ RSpec.describe "Bulk Discount New Page", type: :feature do
     expect(page).to have_content("50% off purchases of 30 or more items.").once
   end
 end
-
-
-# As a merchant
-# When I visit my bulk discounts index
-# Then I see a link to create a new discount
-# When I click this link
-# Then I am taken to a new page where I see a form to add a new bulk discount
-# When I fill in the form with valid data
-# Then I am redirected back to the bulk discount index
-# And I see my new bulk discount listed
