287ba06d09bbb91f5150f4b83b5b8fd6b40d012a
Ethan Black <ethanrossblack@gmail.com>
2023-08-06 20:18:34 -0600

    feat: .best_discount class method for BulkDiscount

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -2,4 +2,8 @@ class BulkDiscount < ApplicationRecord
   belongs_to :merchant
 
   validates :discount, :quantity, presence: true
+
+  def self.best_discount(item_quantity)
+    BulkDiscount.where("quantity <= #{item_quantity}").order(discount: :desc).first
+  end
 end
