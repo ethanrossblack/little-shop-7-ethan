3e96ea7cab7a5a03083b85dba524231cd665d74d
Ethan Black <ethanrossblack@gmail.com>
2023-08-06 20:31:44 -0600

    feat: add merchant+ bulk_discount relationships to invoice_item

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,6 +2,8 @@ class InvoiceItem < ApplicationRecord
   enum :status, { pending: 0, packaged: 1, shipped: 2 }
   belongs_to :item
   belongs_to :invoice
+  has_one :merchant, through: :item
+  has_many :bulk_discounts, through: :merchant
 
   def price_to_currency
     ActiveSupport::NumberHelper::number_to_currency(unit_price.to_f / 100)
