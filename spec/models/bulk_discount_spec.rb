57ff1b2fa145176e452b081bd5a9f4835ff110c5
Ethan Black <ethanrossblack@gmail.com>
2023-08-07 18:06:28 -0600

    fix: DRY up test data

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -19,27 +19,14 @@ RSpec.describe BulkDiscount, type: :model do
     @discount_2a = create(:bulk_discount, discount: 30, quantity: 15, merchant_id: @merchant_a.id)
     @discount_3a = create(:bulk_discount, discount: 15, quantity: 6, merchant_id: @merchant_a.id)
     @discount_4a = create(:bulk_discount, discount: 10, quantity: 20, merchant_id: @merchant_a.id)
-
-    # Items
-    @item_1a = create(:item, merchant_id: @merchant_a.id, unit_price: 10000)
-    @item_2a = create(:item, merchant_id: @merchant_a.id, unit_price: 1000)
-    @item_3a = create(:item, merchant_id: @merchant_a.id, unit_price: 5000)
-
-    # Invoice
-    @invoice_1 = create(:invoice)
-
-    # Invoice Items
-    @invoice_item_1_1a = create(:invoice_item, quantity: 10, unit_price: @item_1a.unit_price, item_id: @item_1a.id, invoice_id: @invoice_1.id)
-    @invoice_item_1_2a = create(:invoice_item, quantity: 5, unit_price: @item_2a.unit_price, item_id: @item_2a.id, invoice_id: @invoice_1.id)
-    @invoice_item_1_3a = create(:invoice_item, quantity: 15, unit_price: @item_3a.unit_price, item_id: @item_3a.id, invoice_id: @invoice_1.id)
   end
 
   describe "Class Methods" do
     describe ".best_discount" do
-      it "returns the best discount for a provided invoice_item" do
-        expect(BulkDiscount.best_discount(@invoice_item_1_1a.quantity)).to eq(@discount_1a)
-        expect(BulkDiscount.best_discount(@invoice_item_1_2a.quantity)).to eq(nil)
-        expect(BulkDiscount.best_discount(@invoice_item_1_3a.quantity)).to eq(@discount_2a)
+      it "returns the best discount for a provided item_quantity" do
+        expect(BulkDiscount.best_discount(12)).to eq(@discount_1a)
+        expect(BulkDiscount.best_discount(5)).to eq(nil)
+        expect(BulkDiscount.best_discount(20)).to eq(@discount_2a)
       end
     end
   end
